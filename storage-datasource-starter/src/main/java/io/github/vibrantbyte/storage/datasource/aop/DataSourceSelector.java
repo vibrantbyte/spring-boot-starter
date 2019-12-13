da105e0223e825ab9b5602dba781456bd8a24d70
vibrantbyte <codingrun@163.com>
2019-12-13 20:40:41 +0800

    增加动态获取数据库链接参数

--- storage-datasource-starter/src/main/java/io/github/vibrantbyte/storage/datasource/aop/DataSourceSelector.java
+++ storage-datasource-starter/src/main/java/io/github/vibrantbyte/storage/datasource/aop/DataSourceSelector.java
@@ -3,6 +3,10 @@ package io.github.vibrantbyte.storage.datasource.aop;
 import java.lang.annotation.*;
 
 /**
+ *
+ * 如果使用 #{} 表示需要从参数中获取，如果不是直接使用这个值
+ * JDK代理，不能获取接口中的参数。
+ *
  * @author xiaoyueya
  */
 @Target({ElementType.METHOD })
--- storage-datasource-starter/src/main/java/io/github/vibrantbyte/storage/datasource/aop/DataSourceSelectorAspect.java
+++ storage-datasource-starter/src/main/java/io/github/vibrantbyte/storage/datasource/aop/DataSourceSelectorAspect.java
@@ -2,11 +2,18 @@ package io.github.vibrantbyte.storage.datasource.aop;
 
 import io.github.vibrantbyte.storage.datasource.DynamicDataSourceHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author vibrant byte
@@ -16,6 +23,15 @@ import org.springframework.core.annotation.Order;
 @Order(2)
 public class DataSourceSelectorAspect {
 
+    /**
+     * 占位符
+     */
+    private static final String SEAT = "#";
+    /**
+     * 正则匹配 字母，数字或者_
+     */
+    private static final String PATTERN = "\\w+";
+
     @Before("@annotation(selector)")
     public void changeDataSource(JoinPoint point,DataSourceSelector selector){
         log.debug("selector change data source :{} > {}", selector.name(), point.getSignature());
@@ -23,7 +39,35 @@ public class DataSourceSelectorAspect {
             DynamicDataSourceHolder.setMasterRouteOnly(selector.mandatoryMaster());
             log.debug("data source  mandatory master :{} > {}", selector.name(), point.getSignature());
         }
-        DynamicDataSourceHolder.setCurrentGroupName(selector.name());
+
+        // 将参数值设置到注解属性上
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        String sourceName;
+        if(StringUtils.isNotEmpty(sourceName = selector.name()) && selector.name().indexOf(SEAT) > -1){
+            Pattern pattern = Pattern.compile(PATTERN);
+            Matcher matcher = pattern.matcher(selector.name());
+            while (matcher.find()) {
+                sourceName = matcher.group();
+                if (StringUtils.isNotEmpty(sourceName)){
+                    break;
+                }
+            }
+            log.info("动态数据源切换获取参中的数据库名称参数为：{}",sourceName);
+            //获取参数名称集合
+            String[] parameterNames = signature.getParameterNames();
+            if (null != parameterNames && parameterNames.length > 0){
+                for (int i = 0; i < parameterNames.length; i++) {
+                    String parameterName = parameterNames[i];
+                    if (sourceName.equals(parameterName)) {
+                        //动态切换
+                        DynamicDataSourceHolder.setCurrentGroupName(point.getArgs()[i].toString());
+                        log.info("动态数据源切换获取参中的数据库名称为:{},切换成功。",point.getArgs()[i]);
+                    }
+                }
+            }
+        }else{
+            DynamicDataSourceHolder.setCurrentGroupName(selector.name());
+        }
     }
 
     @After("@annotation(selector)")
